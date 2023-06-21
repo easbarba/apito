9d59c0c9fd5b676efa463f40e6cb96b2c4aee69b
easbarba <easbarba@outlook.com>
2023-06-21 11:52:27 -0300

    terraform,ansible, and more stack info

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,3 +1,17 @@
+# Apito is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Apito is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Apito. If not, see <https://www.gnu.org/licenses/>.
+
+
 resource "aws_instance" "apito" {
   ami           = var.ami_id
   instance_type = "t2.micro"
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,6 +1,19 @@
+# Apito is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Apito is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Apito. If not, see <https://www.gnu.org/licenses/>.
+
 resource "aws_security_group" "apito-sg-ports-ssh" {
   name        = "apito-sg-ssh"
-  description = "Allow ssh"
+  description = "Allow SSH port"
 
   ingress {
     description = "ssh"
@@ -20,7 +33,7 @@ resource "aws_security_group" "apito-sg-ports-ssh" {
 
 resource "aws_security_group" "apito-sg-ports-server" {
   name        = "apito-sg-ports-server"
-  description = "Allow HTTP"
+  description = "Allow Application Port"
 
   ingress {
     from_port   = 5000
@@ -39,7 +52,7 @@ resource "aws_security_group" "apito-sg-ports-server" {
 
 resource "aws_security_group" "apito-sg-ports-http" {
   name        = "apito-sg-http"
-  description = "Allow HTTP"
+  description = "Allow HTTP port"
 
   ingress {
     description = "http"
@@ -59,7 +72,7 @@ resource "aws_security_group" "apito-sg-ports-http" {
 
 resource "aws_security_group" "apito-sg-ports-https" {
   name        = "apito-sg-https"
-  description = "Allow HTTPS"
+  description = "Allow HTTPS port"
 
   ingress {
     description = "https"
